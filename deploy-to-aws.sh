a4049d68b3e2736fed6d444d7f06d411e305f70b
Chris Howard <chrisau233@gmail.com>
2021-07-14 11:30:28 +1000

    Pull changes on all repos

--- deploy-to-aws.sh
+++ deploy-to-aws.sh
@@ -1,1 +1,18 @@
-echo 'Success'
+# Navigate to repo folder
+cd projects/portfolio-ui
+# Discard any local unsaved changes (package-lock.json usually). These prevent 'git pull' 
+git checkout -- .
+# Pull updates
+git pull
+
+cd projects/portfolio-api
+git checkout -- .
+git pull
+
+cd projects/portfolio-db
+git checkout -- .
+git pull
+
+cd projects/portfolio-deploy
+git checkout -- .
+git pull
